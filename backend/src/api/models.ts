2fda7d796141079272a5770b034655a3a618b342
karamvirsingh98 <karamvir.singh98@gmail.com>
2022-01-13 17:55:17 -0800

    update mongoose models

--- backend/src/api/models.ts
+++ backend/src/api/models.ts
@@ -1,22 +1,32 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-export const EventModel = mongoose.model("olympiad-event", new mongoose.Schema({
-  owner: String,
-  name: String,
-  cases: Array,
-  timers: Array
-}))
+export const EventModel = mongoose.model(
+  "olympiad-event",
+  new mongoose.Schema({
+    owner: String,
+    title: String,
+    cases: [{ case1: String, case2: String }],
+    timers: Array,
+  })
+);
 
-export const CaseModel = mongoose.model("olympiad-case", new mongoose.Schema({
-  owner: String,
-  isVideo: Boolean,
-  videoURL: String,
-  bodyText: String,
-}))
+export const CaseModel = mongoose.model(
+  "olympiad-case",
+  new mongoose.Schema({
+    owner: String,
+    title: String,
+    isVideo: Boolean,
+    videoURL: String,
+    bodyText: String,
+  })
+);
 
-export const UserModel = mongoose.model("users", new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  admin: Boolean,
-}))
+export const UserModel = mongoose.model(
+  "users",
+  new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    admin: Boolean,
+  })
+);
