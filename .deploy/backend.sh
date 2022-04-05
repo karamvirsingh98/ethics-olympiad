af09af6d4af3c36a7a3cc0dbda6e1028c3ce9b00
Karamvir Singh <karamvir.singh98@gmail.com>
2022-04-04 23:48:02 -0700

    better sh commens

--- .deploy/backend.sh
+++ .deploy/backend.sh
@@ -3,9 +3,18 @@ yarn workspace @ethics-olympiad/backend run predeploy
 echo ===== Predeploy Completed =====
 
 # push backend/dist to deployment branch
+echo ===== Initiating Deployment =====
+
+# split subtree
 git subtree split -P backend/build -b deploy-backend
+
+# prepare subtree branch for deployment
 git add -A 
 git commit -m 'Deploying Backend'
+
+# push subtree branch to origin to trigger heroku build
 git push origin -f deploy-backend:deploy-backend
+
+# cleanup local branch
 git branch -D deploy-backend
 echo ===== Pushed to Deployment Branch =====
