7bf4e5a98a564ea53519764d661bb0e42bc1b0a4
Karamvir Singh <karamvir.singh98@gmail.com>
2022-04-05 01:53:11 -0700

    update manager endpoint

--- manager/src/main.tsx
+++ manager/src/main.tsx
@@ -11,7 +11,7 @@ const MAX_HOME_IP = "http://10.0.0.43:3030";
 const LOCALHOST = "http://localhost:3030";
 const HEROKU = "https://ethicsolympiad.herokuapp.com/";
 
-export const client = setupClient(LOCALHOST);
+export const client = setupClient(HEROKU);
 
 ReactDOM.render(
   <React.StrictMode>
