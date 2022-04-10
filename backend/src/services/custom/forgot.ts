bb9a09486b818e463770432f96b5862a4a1f4480
Karamvir Singh <karamvir.singh98@gmail.com>
2022-04-10 09:58:42 -0700

    err handle

--- backend/src/services/custom/forgot.ts
+++ backend/src/services/custom/forgot.ts
@@ -12,7 +12,11 @@ export class ForgotPasswordService {
     const users = this.app.service("api/users");
     const user: User = await users.find({ query: { email } });
     console.log(user);
-    await users.patch(user._id, { email, password });
+    try {
+      await users.patch(user._id, { email, password });
+    } catch (e) {
+      console.log(e);
+    }
     console.log(`user ${email} updated with ${password}`);
     return "password updated";
   }
