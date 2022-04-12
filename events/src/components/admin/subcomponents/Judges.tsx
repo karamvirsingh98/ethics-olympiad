b72702a8b4595231bfcc555f5e5fdff8c8b73c46
Karamvir Singh <karamvir.singh98@gmail.com>
2022-04-11 21:44:52 -0700

    scrolling

--- events/src/components/admin/subcomponents/Judges.tsx
+++ events/src/components/admin/subcomponents/Judges.tsx
@@ -6,14 +6,23 @@ export default function Judges({ judges }: { judges: EventStatus }) {
   const names = Object.keys(judges);
 
   return (
-    <div style={{ overflow: "hidden"}}>
+    <div style={{ overflowY: "scroll", maxHeight: "70vh" }}>
       <ObjectMap
         object={judges}
         map={(name) => (
-          <div style={{ display: "grid", gap: "1rem", alignItems: 'center', overflowX: "scroll", paddingBottom: "1rem" }} key={name}>
+          <div
+            style={{
+              display: "grid",
+              gap: "1rem",
+              alignItems: "center",
+              overflowX: "scroll",
+              paddingBottom: "1rem",
+            }}
+            key={name}
+          >
             <div style={{ display: "flex", gap: "1rem", alignItems: "center" }}>
               <div style={{ fontSize: "2rem" }}> {name} </div>
-              <div style={{ flexWrap: "nowrap"}}>
+              <div style={{ flexWrap: "nowrap" }}>
                 Heat {judges[name].heatNumber}, Round {judges[name].roundNumber}
               </div>
             </div>
