be7f2ccc20651ab11fe8fbcb737be19c08e410e6
Karamvir Singh <karamvir.singh98@gmail.com>
2022-04-18 20:34:51 -0700

    update event and types

--- events/src/components/event/heat/Round.tsx
+++ events/src/components/event/heat/Round.tsx
@@ -1,5 +1,6 @@
 import { Case } from "@ethics-olympiad/types";
 import { Route, Routes, useParams } from "react-router-dom";
+import IfElse from "../../util/IfElse";
 import CaseOverview from "./CaseOverview";
 import Stage from "./Stage";
 
@@ -16,23 +17,47 @@ export default function Round({
   const round = Number(roundNumber);
 
   return (
-    <div style={{ overflow: "hidden"}}>
+    <div style={{ overflow: "hidden" }}>
       <Routes>
-        <Route
-          path="/"
-          element={<CaseOverview _case={round === 1 ? case1 : case2} />}
-        />
-        <Route
-          path="/stage:stageNumber"
-          element={
-            <Stage
-              roundNumber={Number(roundNumber)}
-              question={round === 1 ? case1.question : case2.question}
-              timers={timers}
+        <IfElse
+          showIf={}
+          showTrue={
+            <>
+              <Route
+                path="/"
+                element={<CaseOverview _case={round === 1 ? case1 : case2} />}
+              />
+              <Route
+                path="/stage:stageNumber"
+                element={
+                  <Stage
+                    roundNumber={Number(roundNumber)}
+                    question={round === 1 ? case1.question : case2.question}
+                    timers={timers}
+                  />
+                }
+              />
+            </>
+          }
+          showFalse={
+            <Route
+              path="/"
+              element={<CaseOverview _case={round === 1 ? case1 : case2} />}
             />
           }
         />
       </Routes>
+    </div>
+  );
+}
+
+function NotFound() {
+  return (
+    <div style={{ display: "grid", placeItems: "center" }}>
+      <div style={{ display: "grid", placeItems: "center" }}>
+        This Case was Not properly assigned, or there was an error loading this
+        case.
+      </div>
     </div>
   );
 }
