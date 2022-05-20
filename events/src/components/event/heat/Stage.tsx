af2d2aa74c62af55a66d564c382e83e3e36812a6
Karamvir Singh <karamvir.singh98@gmail.com>
2022-05-19 21:37:05 -0700

    questions can scroll

--- events/src/components/event/heat/Stage.tsx
+++ events/src/components/event/heat/Stage.tsx
@@ -1,4 +1,5 @@
 import useActiveStage from "../../../state/hooks/useActiveStage";
+import { usePadQuestion } from "../../../util/hooks";
 import RoundTracker from "../util/RoundTracker";
 import Timer from "../util/Timer";
 
@@ -11,6 +12,7 @@ export default function Stage({
   timers: number[];
 }) {
   const stage = useActiveStage();
+  const { heightRef, pad } = usePadQuestion();
 
   const fontSize = question.length < 150 ? "3.5rem" : "3rem";
 
@@ -18,7 +20,18 @@ export default function Stage({
     <div className="stage">
       {<RoundTracker stage={stage} showButtons />}
       <div className="stage-content">
-        <div style={{ fontSize }}>{question}</div>
+        <div
+          style={{
+            fontSize,
+            maxHeight: "70vh",
+            overflow: "auto",
+            paddingRight: pad ? "0.5rem" : undefined,
+          }}
+          id="question"
+          ref={heightRef}
+        >
+          {question}
+        </div>
         <Timer duration={timers[stage - 1]} />
       </div>
     </div>
