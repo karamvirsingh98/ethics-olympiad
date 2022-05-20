35378d4a9530bdc5a02a17c5a14b3f77ebb7a2c2
Karamvir Singh <karamvir.singh98@gmail.com>
2022-05-19 21:38:13 -0700

    fix animation

--- events/src/components/util/Chevron.tsx
+++ events/src/components/util/Chevron.tsx
@@ -11,10 +11,11 @@ export default function Chevron({ up }: { up: boolean }) {
       viewBox="0 0 24 24"
       fill="none"
       stroke={dark ? "#f8f4e3" : "#262726"}
-      stroke-width="3"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      transform={`rotate(${up ? "180deg" : "0deg"})`}
+      strokeWidth="3"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      transform={`rotate(${up ? "0" : "-90"})`}
+      style={{ transition: ".2s ease"}}
     >
       <polyline points="6 9 12 15 18 9"></polyline>
     </svg>
