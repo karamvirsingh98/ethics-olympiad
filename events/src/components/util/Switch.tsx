bf4acc6be890bc98ab3abe957b884ed391a40573
Karamvir Singh <karamvir.singh98@gmail.com>
2022-05-19 21:39:05 -0700

    add disabled feature

--- events/src/components/util/Switch.tsx
+++ events/src/components/util/Switch.tsx
@@ -1,9 +1,11 @@
 export default function Switch({
   active,
   onClick,
+  disabled
 }: {
   active: boolean;
   onClick: () => void;
+  disabled?: boolean;
 }) {
   return (
     <div
@@ -13,15 +15,16 @@ export default function Switch({
         height: "1rem",
         borderRadius: "1rem",
         backgroundColor: active ? "green" : "red",
+        ...(disabled ? { cursor: "not-allowed", filter: "brightness(.5)"} : {})
       }}
-      onClick={onClick}
+      onClick={disabled ? undefined : onClick}
     >
       <div
         style={{
           position: "absolute",
           left: 0,
           transform: active ? "translateX(100%)" : "translateX(0%)",
-          transition: "transform .2s",
+          transition: "transform .2s ease",
           borderRadius: "100%",
           width: "1rem",
           height: "1rem",
