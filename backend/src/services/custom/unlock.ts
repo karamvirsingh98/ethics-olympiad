b592f27a4474fa4e52724b996365e3d73608c96b
karamvirsingh98 <67458484+karamvirsingh98@users.noreply.github.com>
2022-06-18 20:32:17 -0700

    extend service to use custom questions

--- backend/src/services/custom/unlock.ts
+++ backend/src/services/custom/unlock.ts
@@ -1,6 +1,12 @@
 import { Application } from "@feathersjs/feathers";
 import { arrToKeyedObject } from "../../helpers";
-import { Event, Template } from "@ethics-olympiad/types";
+import {
+  Case,
+  CustomQuestion,
+  Event,
+  Template,
+  User,
+} from "@ethics-olympiad/types";
 import { Forbidden } from "@feathersjs/errors";
 
 export class UnlockService {
@@ -16,7 +22,7 @@ export class UnlockService {
   ) {
     const { _id, templateID, eventTitle, teams, password }: Event =
       await this.app.service("api/events").get({ _id: data.id });
-    const { templateTitle, heats, timers }: Template = await this.app
+    const { templateTitle, heats, timers, owner }: Template = await this.app
       .service("api/templates")
       .get({ _id: templateID });
     const caseIDs = heats
@@ -25,14 +31,37 @@ export class UnlockService {
         heat.case2 !== "" && heat.case2,
       ])
       .flat();
-    const cases = await this.app
+    let cases = await this.app
       .service("api/cases")
       .find({ query: { _id: { $in: caseIDs } } });
+    const isOfficial = await this.isOfficialEvent(owner);
+    if (!isOfficial) {
+      const customQuestions = await this.app
+        .service("api/questions")
+        .find({ query: { caseID: { $in: caseIDs }, userID: owner } });
+      cases = this.asembleCasesForUnoffficialEvent(cases, customQuestions);
+    }
     if (data.password === password || user) {
       return {
         event: { _id, eventTitle, templateTitle, heats, timers, teams },
         cases: arrToKeyedObject(cases),
       };
     } else throw new Forbidden("invalid password");
+  }
+
+  async isOfficialEvent(userID: string) {
+    const user: User = await this.app.service("api/users").get(userID);
+    return user.admin ? true : false;
+  }
+
+  asembleCasesForUnoffficialEvent(
+    cases: Case[],
+    customQuestions: CustomQuestion[]
+  ) {
+    const cQs = arrToKeyedObject(customQuestions, "caseID");
+    return cases.map((c) => ({
+      ...c,
+      question: cQs[c._id!].question || "no question set",
+    }));
   }
 }
