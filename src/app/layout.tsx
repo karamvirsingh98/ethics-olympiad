b857416eeb819df32bd78063a836a5f5057acd4f
kv <karamvir.singh98@gmail.com>
2024-10-18 01:15:19 -0700

    analytics, speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter as Font } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Navbar } from "@/components/navbar";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import { cookies } from "next/headers";
@@ -32,6 +34,8 @@ export default async function RootLayout({
           <Navbar authenticated={authenticated} />
           <div className="py-12 container flex flex-col gap-12">{children}</div>
         </ThemeProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
