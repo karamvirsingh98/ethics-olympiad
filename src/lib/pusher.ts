a33ef091c0445e5baec42edfc4b021d2c78d0451
kv <karamvir.singh98@gmail.com>
2024-10-18 00:53:59 -0700

    fix filename

---
+++ src/lib/pusher.ts
@@ -0,0 +1,33 @@
+"use client";
+
+import { atom, useAtom } from "jotai";
+import { useEffect } from "react";
+import Pusher from "pusher-js";
+import { PUSHER_FORMATS } from "./utils";
+
+const pusher_atom = atom(
+  new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
+    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
+    authEndpoint: "/api/pusher-auth",
+    authTransport: "ajax",
+    auth: {
+      headers: {
+        Accept: "application/json",
+        Authorization: "Bearer ",
+      },
+    },
+  })
+);
+
+export const usePusher = (eventId: number) => {
+  const [pusher] = useAtom(pusher_atom);
+
+  useEffect(() => {
+    pusher.subscribe(PUSHER_FORMATS.OLYMPIAD_CHANNEL(eventId));
+    return () => {
+      pusher.unsubscribe(PUSHER_FORMATS.OLYMPIAD_CHANNEL(eventId));
+    };
+  }, [pusher, eventId]);
+
+  return pusher;
+};
