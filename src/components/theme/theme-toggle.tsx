c5186f6906ca73d1357fec79dbecfdcc2c8ab360
kv <karamvir.singh98@gmail.com>
2024-10-20 17:00:42 -0700

    cleanup navbar styles

--- src/components/theme/theme-toggle.tsx
+++ src/components/theme/theme-toggle.tsx
@@ -3,8 +3,9 @@
 import * as React from "react";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
-
 import { Select, SelectContent, SelectItem, SelectTrigger } from "../ui/select";
+import { buttonVariants } from "../ui/button";
+import { cn } from "@/lib/utils";
 
 export const ThemeToggle = () => {
   const { theme, setTheme } = useTheme();
@@ -13,16 +14,25 @@ export const ThemeToggle = () => {
     <Select defaultValue="system" value={theme} onValueChange={setTheme}>
       <SelectTrigger
         hideIcon
-        className="p-0 w-9 flex justify-center focus-visible:ring-0 hover:bg-accent/50"
+        className={cn(
+          buttonVariants({ variant: "ghost", size: "icon" }),
+          "p-0 border-none shadow-none focus:ring-0 focus-visible:ring-0"
+        )}
       >
         <SunIcon className="w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
         <MoonIcon className="absolute w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         <span className="sr-only">Toggle theme</span>
       </SelectTrigger>
-      <SelectContent align="end" className="text-sm">
-        <SelectItem value="light">Light</SelectItem>
-        <SelectItem value="dark">Dark</SelectItem>
-        <SelectItem value="system">System</SelectItem>
+      <SelectContent align="start">
+        <SelectItem value="light" className="text-xs">
+          Light
+        </SelectItem>
+        <SelectItem value="dark" className="text-xs">
+          Dark
+        </SelectItem>
+        <SelectItem value="system" className="text-xs">
+          System
+        </SelectItem>
       </SelectContent>
     </Select>
   );
